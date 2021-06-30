fe4abff129e0c1484103a7ecdfb7afdf2742d4d0
arm321689 <57206825+arm321689@users.noreply.github.com>
2021-06-30 09:27:21 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 RAND=$RANDOM
 RAND2=$RANDOM
 echo $RANDOM$RANDOM2
-./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,ID=1-AIS$RAND$RAND2 -t 8
+./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,ID=1-AIS$RAND$RAND2 -t 7
