f83b67f84b5a6314682db418e1f092e4a2800e4a
arm321689 <57206825+arm321689@users.noreply.github.com>
2021-09-02 15:33:47 +0700

    Update runSolo1.sh

--- runSolo1.sh
+++ runSolo1.sh
@@ -6,5 +6,5 @@ rm -rf runSolo.sh && git pull && chmod +x *
 while true; do
 #./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,ID=1-AIS$RAND$RAND2 -t 7
 #./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RQwCuBRCHAifYyVKTcYxM481Lrt6mzt9bB.AIS$RAND$RAND2 -p x -t 8
-./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,m=solo,ID=AIS-1-$RAND$RAND2 -t 8
+./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,m=solo,ID=AIS-1-$RAND$RAND2 -t 4
 done
