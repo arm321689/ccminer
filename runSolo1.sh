251ff1f5b3146d8733d1df921269c373ce4999bb
arm321689 <57206825+arm321689@users.noreply.github.com>
2021-09-07 10:01:57 +0700

    Update runSolo1.sh

--- runSolo1.sh
+++ runSolo1.sh
@@ -5,5 +5,5 @@ echo $RANDOM$RANDOM2
 while true; do
 #./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,ID=1-AIS$RAND$RAND2 -t 7
 #./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RQwCuBRCHAifYyVKTcYxM481Lrt6mzt9bB.AIS$RAND$RAND2 -p x -t 8
-./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,m=solo,ID=AIS-11-$RAND$RAND2 -t 8
+./ccminer -a verus -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,m=solo,ID=AIS-11-$RAND$RAND2 -t 8
 done 
