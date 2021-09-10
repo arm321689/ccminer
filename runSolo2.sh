ee8ba78793682e8b80ff56e4fa0398b3bb20f13d
arm321689 <57206825+arm321689@users.noreply.github.com>
2021-09-10 15:45:34 +0700

    Create runSolo2.sh

---
+++ runSolo2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+RAND=$RANDOM
+RAND2=$RANDOM
+echo $RANDOM$RANDOM2
+while true; do
+#./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,ID=1-AIS$RAND$RAND2 -t 7
+#./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RQwCuBRCHAifYyVKTcYxM481Lrt6mzt9bB.AIS$RAND$RAND2 -p x -t 8
+./ccminer -a verus -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,m=party.doibtcethdoge,ID=AIS-22-$RAND$RAND2 -t 8
+done 
