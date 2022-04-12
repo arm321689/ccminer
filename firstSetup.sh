43431d3309de835a7fe5a332e941e83ede50b5a5
arm321689 <57206825+arm321689@users.noreply.github.com>
2022-04-12 23:37:11 +0700

    Update firstSetup.sh

--- firstSetup.sh
+++ firstSetup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get update && apt-get upgrade -y && apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git nano -y && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && chmod +x run.sh && ./build.sh
+apt-get update && apt-get upgrade -y  --fix-missing && apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git nano -y && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && chmod +x run.sh && ./build.sh
