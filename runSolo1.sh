cfcf1d3f089c10078946bf69aeaaf89d62fc91e7
arm321689 <57206825+arm321689@users.noreply.github.com>
2022-06-01 00:19:44 +0700

    Update runSolo1.sh

--- runSolo1.sh
+++ runSolo1.sh
@@ -10,7 +10,7 @@ while true; do
 #./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,ID=1-AIS$RAND$RAND2 -t 7
 #./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RQwCuBRCHAifYyVKTcYxM481Lrt6mzt9bB.AIS$RAND$RAND2 -p x -t 8
 #./ccminer -a verus -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,m=party.doibtcethdoge,ID=AIS-1$i-$ip4 -t 8
-./ccminer -a verus -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,ID=AIS-1$i-$ip4 -t 8 -i 9
+./ccminer -a verus -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DSsZzhocDVibrpR93BMjwjaQRLZ3ftpMHF -p c=DOGE,ID=AIS-1$i-$ip4 -t 8 --cpu-affinity 254 --cpu-priority 3 -i 9
 
 ((i=i+1))
 done 
